f03ab7f90efc56501750b23b9dff74ae9667fd48
Renan Carneiro <renanfigcarneiro@gmail.com>
2023-05-11 14:39:41 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Renan Carneiro on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
