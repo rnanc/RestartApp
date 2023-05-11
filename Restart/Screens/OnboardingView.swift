3e19186f93a26ea004be27bf22b3b33c22127e29
Renan Carneiro <renanfigcarneiro@gmail.com>
2023-05-11 18:14:13 -0300

    initial commit

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,129 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Renan Carneiro on 11/05/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    @State private var isAnimating: Bool = false
+
+    var body: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+               Spacer()
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                }
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                ZStack {
+                    CircleGroupView(shapeColor: .white,
+                                    shapeOpacity: 0.2)
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                Spacer()
+                
+                ZStack {
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3,
+                                      design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: buttonOffset + 80)
+                        Spacer()
+                    }
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24,
+                                              weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80,
+                               height: 80,
+                           alignment: .center)
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if gesture.translation.width > 0  && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    withAnimation(Animation.easeOut(duration: 0.4)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        } else {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                })
+                        )
+                        
+                        Spacer()
+                    }
+                }
+                .frame(width: buttonWidth,
+                       height: 80,
+                       alignment: .center)
+                .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+            }
+        }
+        .onAppear {
+            isAnimating = true
+        }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
