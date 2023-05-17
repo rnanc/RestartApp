eb5d00b039ea2bb8406230c5d71c0cdd2aa6cd67
Renan Carneiro <renanfigcarneiro@gmail.com>
2023-05-17 12:19:29 -0300

    done

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,6 +13,8 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var opacityIndication: Double = 1.0
 
     var body: some View {
         ZStack {
@@ -43,9 +45,44 @@ struct OnboardingView: View {
                 ZStack {
                     CircleGroupView(shapeColor: .white,
                                     shapeOpacity: 0.2)
+                    .offset(x: imageOffset.width * -1)
+                    .blur(radius: abs(imageOffset.width / 5))
+                    .animation(.easeOut(duration: 1), value: imageOffset)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            opacityIndication = 0
+                                        }
+                                    }
+                                }
+                                .onEnded { _ in
+                                    imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        opacityIndication = 1
+                                    }
+                                }
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                        .overlay(
+                            Image(systemName: "arrow.left.and.right.circle")
+                                .font(.system(size: 44, weight: .ultraLight))
+                                .foregroundColor(.white)
+                                .offset(y: 20)
+                                .opacity(isAnimating ? 1 : 0)
+                                .opacity(opacityIndication)
+                                .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                            , alignment: .bottom
+                        )
                 }
                 Spacer()
                 
@@ -103,7 +140,6 @@ struct OnboardingView: View {
                                     }
                                 })
                         )
-                        
                         Spacer()
                     }
                 }
@@ -119,6 +155,7 @@ struct OnboardingView: View {
         .onAppear {
             isAnimating = true
         }
+        .preferredColorScheme(.dark)
     }
 }
 
